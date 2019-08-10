51c69b109b1490c1e3757a07dd6074a3e7ccc710
Donal tun <americanarakan@gmail.com>
2019-08-10 06:54:53 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -15,7 +15,9 @@ function ageChecker(age) {
   if (age >=13 && age <= 19){
 return "You are a teenager!"
   }
-
+  if (age <= 12){
+    return "You are a kid"
+  }
 }
 
 function ternaryTeenager(age) {
